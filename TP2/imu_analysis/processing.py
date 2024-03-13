5495c3545cca4234615ee751ddc0f0271b907ee7
Seimon MILLET <simon.millet@uha.fr>
2024-03-13 11:33:24 +0100

    moving average code added

--- TP2/imu_analysis/processing.py
+++ TP2/imu_analysis/processing.py
@@ -2,3 +2,11 @@
 Module pour le traitement des mesures
 *************************************
 """
+
+
+def calculate_moving_average(data: list[float], interval: int):
+    mov_ave = 0
+    for i in range(interval):
+        mov_ave += data[i]
+    mov_ave = mov_ave / interval
+    return mov_ave
