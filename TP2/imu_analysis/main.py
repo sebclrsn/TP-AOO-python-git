6d810cfa2d199590df68bd31fc311f7329873731
Kylian Bujat <kylian.bujat@uha.fr>
2024-03-13 11:40:07 +0100

    <Bujat> <Kylian> lissage du signal <ils sont tous lissés(wx,wy,wz,ax,ay,az>

--- TP2/imu_analysis/main.py
+++ TP2/imu_analysis/main.py
@@ -1,6 +1,6 @@
 from plotting import plot_angular_speed, plot_linear_acceleration, show_plots
 from reader import read_measurements_file
-
+from processing import calculate_moving_average
 
 def main():
     """Fonction principale du projet.
@@ -20,6 +20,7 @@ def main():
     az = []
 
     for measurement in read_measurements_file():
+
         t.append(measurement.t)
         wx.append(measurement.wx)
         wy.append(measurement.wy)
@@ -27,9 +28,15 @@ def main():
         ax.append(measurement.ax)
         ay.append(measurement.ay)
         az.append(measurement.az)
+    wx_l=calculate_moving_average(wx,100)
+    wy_l=calculate_moving_average(wy,100)
+    wz_l=calculate_moving_average(wz,100)
+    ax_l=calculate_moving_average(ax,100)
+    ay_l=calculate_moving_average(ay,100)
+    az_l=calculate_moving_average(az,100)
 
-    plot_angular_speed(t, wx, wy, wz)
-    plot_linear_acceleration(t, ax, ay, az)
+    plot_angular_speed(t, wx_l, wy_l, wz_l)
+    plot_linear_acceleration(t, ax_l, ay_l, az_l)
 
     show_plots()
 
