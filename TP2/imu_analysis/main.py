4739169e991fc167b72722f65da5a8b2ea949458
Rayann KHALFI <rayann.khalfi@uha.fr>
2024-03-13 11:47:59 +0100

    <KHALFI><Rayann> lissage du signal

--- TP2/imu_analysis/main.py
+++ TP2/imu_analysis/main.py
@@ -36,3 +36,15 @@ def main():
 
 if __name__ == "__main__":
     main()
+def calculate_moving_average(data: list[float],interval: int):
+    moyennes = []
+    for i in range(len(data) - interval + 1):
+        moyenne = sum(data[i:i+interval]) / interval
+        moyennes.append(moyenne)
+    return moyennes
+
+# Exemple d'utilisation
+data = [0,1,2,3,4,5,6,7,8,9]
+interval = 4
+resultat = moyenne_glissante(data, interval)
+print("Moyennes glissantes:", resultat)
