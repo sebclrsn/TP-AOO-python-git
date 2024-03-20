9061e5bc55e3d5e24e637ee2190996e78481b603
Théo FP <theo.farcasiu-popovici@uha.fr>
2024-03-20 10:37:06 +0100

    Essaie 1

--- TP2/imu_analysis/processing.py
+++ TP2/imu_analysis/processing.py
@@ -5,11 +5,12 @@ Module pour le traitement des mesures
 
 
 def calculate_moving_average(data: list[float], interval: int):
+    n = 4
     moy_g = 0
     N = len(interval)
-    for i in range(0, interval - 1):
-        for j in range(N, i + N):
-            moy_g = (1 / N) * sum(data[i])
-            print(moy_g)
+    for i in range(n, interval + n):
+        moy_g = moy_g + list(n + i)
+    moy_g = 1 / N * moy_g
+    print(moy_g)
     print("JSP si ça fonctionne ")
     return 0
