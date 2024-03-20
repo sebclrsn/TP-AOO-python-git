bb4cb5d3c1ac34005ba9685d4d86ff0c085a93c4
Nekicoul <kylian.bujat@uha.fr>
2024-03-20 11:44:16 +0100

    TP3

--- TP3/person.py
+++ TP3/person.py
@@ -2,18 +2,26 @@
 
 
 class Person:
-    def __init__(self, first_name: str, last_name: str, age: int) -> None:
+    def __init__(self, first_name: str, last_name: str, age: int , mail: str, phone: str) -> None:
         self.first_name = first_name.capitalize()
         self.last_name = last_name.upper()
         self.age = age
+        self.mail= mail
+        self.phone= phone
+    def __str__(self)->str:
+        return f"{self.first_name}  {self.last_name}"
 
 
 def main():
-    john = Person("John", "Doe", 30)
+    john = Person("John", "Doe", 30, "john.doe@uha.fr", "+33 06 82 72 22 76")
     print(f"First name: {john.first_name}")
     print(f"Last name: {john.last_name}")
     print(f"Age: {john.age}")
+    print(f"mail: {john.mail}")
+    print(f"Phone number: {john.phone}")
+    print(john)
 
 
 if __name__ == "__main__":
     main()
+
--- TP3/vectors.py
+++ TP3/vectors.py
@@ -3,6 +3,31 @@
 from __future__ import annotations
 
 
+class Point:
+    def __init__(self, x: float, y: float):
+        self.x=x
+        self.y=y
+    def __str__(self):
+        return f"Point  = ({self.x},{self.y})"
+    
+
+
+class Vector:
+    def __init__(self,A:Point,B:Point):
+        self.x=B.x-A.x
+        self.y=B.y-A.y
+    def produit_scalaire(self,other):
+        prod=self.x*other.x + self.y*other.y
+        return prod
+    def __str__(self):
+        return f"Vecteur = ({self.x},{self.y})"
+    def __add__(self,other):
+        return (self.x+other.x,self.y+other.y)
+    def __sub__(self,other):
+        return (self.x-other.x,self.y-other.y)
+
+
+
 def main():
     origin = Point(0, 0)
     point_A = Point(-2, -1)
@@ -10,12 +35,16 @@ def main():
 
     print(f"Point O: x={origin.x} y={origin.y}")
     print(f"Point A: x={point_A.x} y={point_A.y}")
+    print(point_A)
+
 
     vector_OA = Vector(origin, point_A)
     vector_AB = Vector(point_A, point_B)
 
     print(f"Vector OA: dx={vector_OA.x} dy={vector_OA.y}")
     print(f"Vector AB: dx={vector_AB.x} dy={vector_AB.y}")
+    print(vector_AB)
+    
 
 
 def main_2():
