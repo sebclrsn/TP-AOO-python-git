9a6e328f9e121b29e5a8e4f8527c25c78186494e
LUCAS RUNSER <lucas.runser@uha.fr>
2024-03-20 10:36:53 +0100

    slslsd

--- TP3/person.py
+++ TP3/person.py
@@ -2,10 +2,12 @@
 
 
 class Person:
-    def __init__(self, first_name: str, last_name: str, age: int) -> None:
+    def __init__(self, first_name: str, last_name: str, age: int, email: str = "prenom.nom@mail.com", phone: str = "1234567890") -> None:
         self.first_name = first_name.capitalize()
         self.last_name = last_name.upper()
         self.age = age
+        self.email = email
+        self.phone = phone
 
 
 def main():
@@ -13,6 +15,8 @@ def main():
     print(f"First name: {john.first_name}")
     print(f"Last name: {john.last_name}")
     print(f"Age: {john.age}")
+    print(f"Email: {john.email}")
+    print(f"Phone: {john.phone}")   
 
 
 if __name__ == "__main__":
